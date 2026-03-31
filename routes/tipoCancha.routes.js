53116fcfd2aa90d1278f74aa98c5be0aea3d42ee
MiguelRTx <mariacamiguel962@gmail.com>
2026-03-31 15:24:58 -0400

    CambiosAdmiin

--- routes/tipoCancha.routes.js
+++ routes/tipoCancha.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const tipoCanchaController = require('../controllers/tipoCancha.controller');
 const { isAuth, isAdmin } = require('../middlewares/auth.middleware');
 
-// Todas estas rutas estarán protegidas en el index.js, pero las aseguramos aquí también
+
 router.get('/', isAuth, isAdmin, tipoCanchaController.listar);
 router.get('/create', isAuth, isAdmin, tipoCanchaController.mostrarFormulario);
 router.get('/edit/:id', isAuth, isAdmin, tipoCanchaController.mostrarFormulario);
